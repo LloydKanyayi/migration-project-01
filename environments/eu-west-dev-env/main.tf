2565ba123978c7d491251552162b69fa1ff49961
LloydKanyayi <lloyd.kanyayi@gmail.com>
2021-03-10 23:11:52 +0000

    update these files

---
+++ environments/eu-west-dev-env/main.tf
@@ -0,0 +1,27 @@
+#---------- eu-west-dev-env/main.tf
+
+module "providers" {
+  source = "../../modules/providers"
+  aws_region = var.eu_region
+}
+
+module "networking" {
+  source   = "../../modules/networking"
+  vpc_cidr = "172.0.0.0/16"
+}
+
+
+module "alb" {
+  source         = "../../modules/alb"
+  aws_vpc        = module.networking.vpc_id
+  public_subnets = module.networking.subnet_id_public
+}
+
+module "ecs-fargate" {
+  source = "../../modules/ecs-fargate"
+  aws_alb_target_group_arn = module.alb.aws_alb_target_group_arn
+  private_subnets = module.networking.subnet_id_private
+  vpc_id = module.networking.vpc_id
+
+  alb_listener_front_end = module.alb.alb_listener_front_end
+}
---
+++ environments/eu-west-dev-env/variables.tf
@@ -0,0 +1,5 @@
+#---------- eu-west-dev-env/variables.tf
+
+variable "eu_region" {
+  default = "eu-west-1"
+}
